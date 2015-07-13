ccea5460fa72439431dba2a0f6a4f20cecac51f5
Ian <ian@iancmacdonald.com>
2015-07-13 11:51:31 -0700

    Create question model

--- spec/features/users/login_user_path_spec.rb
+++ spec/features/users/login_user_path_spec.rb
@@ -1,10 +1,19 @@
 require 'rails_helper'
 
 describe 'the user login path' do
-  it 'will log in a user' do
+  it 'will log in a user with username' do
     user = create(:user)
     visit new_user_session_path
     fill_in 'user_login', with: user.username
+    fill_in 'user_password', with: user.password
+    click_on 'Log in'
+    expect(page).to have_content 'successfully'
+  end
+
+  it 'will log in a user with email' do
+    user = create(:user)
+    visit new_user_session_path
+    fill_in 'user_login', with: user.email
     fill_in 'user_password', with: user.password
     click_on 'Log in'
     expect(page).to have_content 'successfully'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,6 @@ require 'rails_helper'
 describe User do
   it { should validate_presence_of :username }
   it { should validate_uniqueness_of :username }
+
+  it { should have_many :questions }
 end
