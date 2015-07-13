5f07a7d9847bc43c4ce97dc84359f2624ca36f20
Ian <ian@iancmacdonald.com>
2015-07-13 11:44:09 -0700

    Use username or email to log in

--- spec/features/users/login_user_path_spec.rb
+++ spec/features/users/login_user_path_spec.rb
@@ -4,7 +4,7 @@ describe 'the user login path' do
   it 'will log in a user' do
     user = create(:user)
     visit new_user_session_path
-    fill_in 'user_email', with: user.email
+    fill_in 'user_login', with: user.username
     fill_in 'user_password', with: user.password
     click_on 'Log in'
     expect(page).to have_content 'successfully'
@@ -13,7 +13,7 @@ describe 'the user login path' do
   it 'will fail to log in a user if information is incorrect' do
     user = create(:user)
     visit new_user_session_path
-    fill_in 'user_email', with: user.email
+    fill_in 'user_login', with: user.username
     fill_in 'user_password', with: user.password + "1"
     click_on 'Log in'
     expect(page).to have_content 'Invalid'
